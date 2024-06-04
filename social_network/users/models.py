1ebd9dcae3adbec2a8e6687d1b5e04dbfae05617
Dawid Nowotny <dawidnowotny@o2.pl>
2024-06-04 09:58:50 +0200

    Updated field in avatar model

--- social_network/users/models.py
+++ social_network/users/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Avatar(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='avatar')
-    image = models.ImageField(upload_to='avatars')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='avatars')
+    image = models.ImageField(upload_to='images/avatars')
     current = models.BooleanField(default=True)
