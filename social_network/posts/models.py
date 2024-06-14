09789122e26a6365ed021d220dff354859f82d4b
Dawid Nowotny <dawidnowotny@o2.pl>
2024-06-14 16:30:29 +0200

    Added PostImage model

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -6,3 +6,7 @@ class Post(models.Model):
     content = models.TextField()
     creation_date = models.DateField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='liked_posts')
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='images/posts')
